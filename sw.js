643f120b50896b7062a9457f9d7443abdd1b5d39
ommojha789-alt <ommojha789@gmail.com>
2026-03-21 08:40:19 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = 'focus-v5'; 
+const assets = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting(); 
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
